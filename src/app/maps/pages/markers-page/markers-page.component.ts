30cee08cd037ac649bd50231408a8e256a1dce65
Victor White <victorblanco281201@gmail.com>
2024-08-21 14:30:18 -0400

    Done until 301 video

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
+
+interface PlainMarker {
+  color: string;
+  lngLat: number[];
+}
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styles: ``
+  styleUrls: ['./markers-page.component.css'],
 })
 export class MarkersPageComponent {
+  @ViewChild('map') divMap?: ElementRef;
 
+  public map?: Map;
+  public currentCenter: LngLat = new LngLat(-66.820668, 10.483112);
+
+  public markers: MarkerAndColor[] = [];
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw new Error('El divMap no está definido');
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentCenter, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    this.loadFromLocalStorage();
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Hola Mundo';
+
+    // const marker: Marker = new Marker({
+    //   // color: '#3887be',
+    //   // element: markerHtml,
+    // })
+    //   .setLngLat(this.currentCenter)
+    //   .addTo(this.map);
+  }
+
+  public createMarker() {
+    if (!this.map) return;
+
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const lngLat = this.map.getCenter();
+
+    this.addMarker(lngLat, color);
+  }
+
+  public addMarker(lngLat: LngLat, color: string = '#3887be') {
+    if (!this.map) return;
+
+    const newMarker: Marker = new Marker({
+      color: color,
+      draggable: true,
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map!);
+
+    //Cuando creo un marcador, le agregamos este listener para ver cuando se mueve
+    newMarker.on('dragend', () => {
+      this.saveToLocalStorage();
+    });
+
+    this.markers.push({ color: color, marker: newMarker });
+
+    this.saveToLocalStorage();
+  }
+
+  public removeMarker(index: number) {
+    this.markers[index].marker.remove(); //Removemos el marker del mapa
+    this.markers.splice(index, 1); //Removemos el marker del arreglo
+
+    this.saveToLocalStorage(); //Actualizamos el storage
+  }
+
+  public flyTo(marker: Marker) {
+    if (!this.map) return;
+
+    this.map.flyTo({
+      zoom: 16,
+      center: marker.getLngLat(),
+      essential: true,
+    });
+  }
+
+  public saveToLocalStorage() {
+    //Hacemos esta transformación para quedarnos solamente con la información que necesitamos
+    const plainMarkers: PlainMarker[] = this.markers.map(
+      ({ color, marker }) => {
+        return {
+          color: color,
+          lngLat: marker.getLngLat().toArray(),
+        };
+      }
+    );
+
+    localStorage.setItem('plainMarkers', JSON.stringify(plainMarkers));
+  }
+
+  public loadFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers');
+    if (!plainMarkersString) return;
+
+    const plainMarkers: PlainMarker[] = JSON.parse(plainMarkersString); //!Ojo pelao con esto
+
+    plainMarkers.forEach(({ color, lngLat }) => {
+      const [lng, lat] = lngLat;
+      this.addMarker(new LngLat(lng, lat), color);
+    });
+  }
 }
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,75 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
-  styles: ``
+  styleUrls: ['./zoom-range-page.component.css'],
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('map') divMap?: ElementRef;
 
+  public map?: Map;
+  public zoomLevel: number = 18;
+  public currentCenter: LngLat = new LngLat(-66.820668, 10.483112);
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw new Error('El divMap no está definido');
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentCenter, // starting position [lng, lat]
+      zoom: this.zoomLevel, // starting zoom
+    });
+
+    this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  //Método que escucha los eventos del mapa (cuando se le hace zoom o se termina de hacer zoom)
+  public mapListeners() {
+    if (!this.map) throw new Error('Mapa no inicializado');
+
+    this.map.on('zoom', (ev) => {
+      this.zoomLevel = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() > 18) {
+        this.map!.zoomTo(18);
+      }
+    });
+
+    this.map.on('move', () => {
+      const { lng, lat } = this.map!.getCenter();
+      this.currentCenter = new LngLat(lng, lat);
+    });
+  }
+
+  //Cuando se le da al boton de -
+  public zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  //Cuando se le da al boton de +
+  public zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  public zoomChanged(value: string) {
+    this.zoomLevel = Number(value);
+    this.map!.zoomTo(this.zoomLevel);
+  }
 }
