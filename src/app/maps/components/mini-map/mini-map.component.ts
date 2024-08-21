f374bc72cdc5344306eaed02ecb5e7455057be59
Victor White <victorblanco281201@gmail.com>
2024-08-21 14:59:36 -0400

    Done Section 21

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
-  styles: ``
+  styleUrls: ['./mini-map.component.css'],
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
 
+  @Input()
+  public lngLat?: [number, number];
+
+  @ViewChild('map') divMap?: ElementRef;
+  public map?: Map;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw new Error('El divMap no está definido');
+    if (!this.lngLat) throw new Error('El atributo lngLat es requerido');
+    console.log(this.lngLat);
+    
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false
+    });
+
+    const marker: Marker = new Marker()
+      .setLngLat(this.lngLat)
+      .addTo(this.map);
+    
+    
+  }
 }
